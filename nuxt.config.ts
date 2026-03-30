80e36af5390949ee7865955fb1084e7377c808e0
ggc <gabriel.galan.17@gmail.com>
2026-03-30 17:28:22 +0200

    [GGC] fix(resume): assets missing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,13 @@
 export default defineNuxtConfig({
   app: {
     baseURL: '/ggc-github-io/',
+    buildAssetsDir: 'assets',
   },
   css: ['@/assets/tui.css', '@/assets/terminal-app.css'],
   modules: ['@nuxt/content', '@pinia/nuxt'],
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
+  nitro: {
+    preset: 'github-pages',
+  },
 })
